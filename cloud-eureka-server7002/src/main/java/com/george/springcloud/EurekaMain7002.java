4c531c999426104e4af79d1e6636537a29865442
YangHao <acloudyh@gmail.com>
2020-09-14 22:33:11 +0800

    feat:本地eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/george/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.george.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-14 21:47
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
