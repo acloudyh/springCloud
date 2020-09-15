7958c40c3728cf28d646d744604aad4796494078
YangHao <acloudyh@gmail.com>
2020-09-15 22:02:21 +0800

    服务提供者向consul注册

---
+++ cloud-providerconsul-payment8006/src/main/java/com/george/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.george.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-15 17:32
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentConsul() {
+        return "springcloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
