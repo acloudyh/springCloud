1a1a920eef466c8e0651f31ce3611953246c111c
YangHao <acloudyh@gmail.com>
2020-09-15 14:21:54 +0800

    使用@LoadBalanced注解赋予RestTemplate负载均衡的能力

--- cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.george.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
+    @LoadBalanced
+    //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
