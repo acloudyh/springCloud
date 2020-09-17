5a11c60ac068f01789b7c2b05bd252d87dbeec8a
YangHao <acloudyh@gmail.com>
2020-09-17 10:17:18 +0800

    format

--- cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,5 @@
 package com.george.springcloud.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
