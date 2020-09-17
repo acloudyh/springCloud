caff2e219ffca8a4812ed82121ed9f2cc10203db
YangHao <acloudyh@gmail.com>
2020-09-17 09:58:47 +0800

    Hystrix初始化构建,服务端

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/george/springcloud/controller/PaymentHystrixController.java
@@ -0,0 +1,39 @@
+package com.george.springcloud.controller;
+
+import com.george.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-17 09:11
+ */
+@RestController
+@Slf4j
+public class PaymentHystrixController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_OK(id);
+        log.info("------------result: " + result);
+        return result;
+    }
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentInfo_TimeOut(id);
+        log.info("------------result: " + result);
+        return result;
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/george/springcloud/service/PaymentService.java
@@ -0,0 +1,42 @@
+package com.george.springcloud.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-16 18:24
+ */
+@Service
+public class PaymentService {
+    /**
+     * 正常访问，肯定ok
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + " PaymenyInfo_OK,id: " + id + "\t" + "O(∩_∩)O哈哈~";
+    }
+
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
+    public String paymentInfo_TimeOut(Integer id) {
+
+        int timeNumber = 3000;
+//         int timeNumber = 10/0;
+        try {
+            TimeUnit.MILLISECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池： " + Thread.currentThread().getName() + " PaymenyInfo_TimeOut,id: " + id + "\t" + "O(∩_∩)O哈哈~" + " 耗时" + timeNumber + "毫秒";
+    }
+
+
+}
