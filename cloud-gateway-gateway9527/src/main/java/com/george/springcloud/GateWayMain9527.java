dde5beaf1b9a2c31f3554e6f24acf9cdc3d3fe9d
YangHao <acloudyh@gmail.com>
2020-09-18 16:50:28 +0800

    Spring Cloud Gateway 配置路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/george/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-18 16:18
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class);
+    }
+}
