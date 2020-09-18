24c45729943abd09717d0a5cfac9e0b26ed36308
YangHao <acloudyh@gmail.com>
2020-09-18 14:20:15 +0800

    Hystrix dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/george/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-18 14:16
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+}
