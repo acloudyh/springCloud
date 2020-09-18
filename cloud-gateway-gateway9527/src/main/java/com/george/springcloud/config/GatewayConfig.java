38f375543b7cc78e68a4024b2c8b4bd9fbae10c8
YangHao <acloudyh@gmail.com>
2020-09-18 17:15:52 +0800

    Gateway 网关路由 代码方式方式配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/george/springcloud/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.george.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Yang Hao
+ * @description 官网 https://cloud.spring.io/spring-cloud-static/spring-cloud-gateway/2.2.1.RELEASE/reference/html/#gatewayfilter-factories
+ * @date 2020-09-18 16:55
+ */
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 配置一个ID为 path_route_george 的路由规则
+     * 当访问地址:http://localhost:9527/guonei 时会自动转发到地址 http://news.baidu.com/guonei
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customeRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("path_route_george", r -> r.path("/guonei").uri("http://news.baidu.com/guonei"))
+                .build();
+    }
+}
