28effcfa89ccd2f8e4812e64c12c9aea1ddc660d
YangHao <acloudyh@gmail.com>
2020-09-20 08:26:12 +0800

    config手动刷新

--- cloud-config-client-3355/src/main/java/com/george/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/george/springcloud/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.george.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
