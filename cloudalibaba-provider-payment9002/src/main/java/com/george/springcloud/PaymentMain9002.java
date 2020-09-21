060f847d05819032a8343956494168b7a10634da
YangHao <acloudyh@gmail.com>
2020-09-21 20:56:14 +0800

    Nacos的服务提供者

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/george/springcloud/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-21 18:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class);
+    }
+}
