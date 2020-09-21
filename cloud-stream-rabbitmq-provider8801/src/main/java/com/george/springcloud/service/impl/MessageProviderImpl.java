353548af10332f523f6c2b32c770f33dc2653c12
YangHao <acloudyh@gmail.com>
2020-09-21 16:31:20 +0800

    log

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/george/springcloud/service/impl/MessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/george/springcloud/service/impl/MessageProviderImpl.java
@@ -30,7 +30,7 @@ public class MessageProviderImpl implements IMessageProvider {
     public String send() {
         String serial = UUID.randomUUID().toString();
         output.send(MessageBuilder.withPayload(serial).build());
-        log.info("------------serial:[{}]", serial);
+        log.info("生产者发送消息 ------> [{}]", serial);
         return null;
     }
 }
