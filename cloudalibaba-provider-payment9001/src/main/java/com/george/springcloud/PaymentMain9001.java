d1cdda373263eb3f8378968646fb6108a00e1e01
YangHao <acloudyh@gmail.com>
2020-09-21 20:52:15 +0800

    nacos 初始化

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/george/springcloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-21 18:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/george/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.george.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-21 18:24
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+
+}
