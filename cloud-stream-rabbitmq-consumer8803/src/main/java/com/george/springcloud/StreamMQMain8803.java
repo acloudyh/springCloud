703dfc02b99de0a3ec3a75e8e8cee6b29dd43653
YangHao <acloudyh@gmail.com>
2020-09-21 16:33:42 +0800

    stream 分组,重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/george/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-21 15:32
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class);
+    }
+}
