55f582d883706eeb2d5b44a6dccd7758db17ea0a
YangHao <acloudyh@gmail.com>
2020-09-23 16:48:43 +0800

    热点限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/george/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/george/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.george.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -52,5 +55,17 @@ public class FlowLimitController {
         return "------testE 测试异常数";
     }
 
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "-------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2,
+                                  BlockException exception){
+        return "----deal_testHotKey,o(╥﹏╥)o";
+
+    }
 
 }
