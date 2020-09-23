3dd7dc314f119a145d1576b40fbbb2d312fdc054
YangHao <acloudyh@gmail.com>
2020-09-23 16:31:50 +0800

    Sentinel 降级测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/george/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/george/springcloud/controller/FlowLimitController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Yang Hao
  * @description
@@ -15,12 +17,31 @@ public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "----------testA";
     }
 
     @GetMapping("/testB")
     public String testB() {
+        log.info("----------testB:[{}]", Thread.currentThread().getName());
         return "----------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age = 10 / 0;
+        return "------testD";
     }
 
 }
