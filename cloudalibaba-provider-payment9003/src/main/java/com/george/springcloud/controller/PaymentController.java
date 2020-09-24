36b9057659c2719660fc1c5e91510eb711b15157
YangHao <acloudyh@gmail.com>
2020-09-24 09:37:04 +0800

    format

--- cloudalibaba-provider-payment9003/src/main/java/com/george/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9003/src/main/java/com/george/springcloud/controller/PaymentController.java
@@ -17,9 +17,6 @@ import java.util.HashMap;
 @RestController
 public class PaymentController {
 
-    @Value("${server.port}")
-    private String serverPort;
-
     public static HashMap<Long, Payment> hashMap = new HashMap<>();
 
     /**
@@ -30,6 +27,9 @@ public class PaymentController {
         hashMap.put(2L, new Payment(2L, "28a4c1e3bc2742d8848569891fb42181"));
         hashMap.put(3L, new Payment(3L, "28a5c1e3bc2742d8848569891fb42181"));
     }
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @GetMapping(value = "/paymentSQL/{id}")
     public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id) {
