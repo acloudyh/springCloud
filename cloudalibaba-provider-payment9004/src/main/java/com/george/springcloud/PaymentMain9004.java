d26f97cb0e00d41de243585e3809b58d5a1e5d82
YangHao <acloudyh@gmail.com>
2020-09-24 09:31:03 +0800

    9004

---
+++ cloudalibaba-provider-payment9004/src/main/java/com/george/springcloud/PaymentMain9004.java
@@ -0,0 +1,18 @@
+package com.george.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Yang Hao
+ * @date 2020-09-24 09:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
+
---
+++ cloudalibaba-provider-payment9004/src/main/java/com/george/springcloud/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.george.springcloud.controller;
+
+
+import com.george.springcloud.entities.CommonResult;
+import com.george.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @author Yang Hao
+ * @date 2020-09-24 09:22
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    public static HashMap<Long, Payment> hashMap = new HashMap<>();
+
+    static {
+        hashMap.put(1L, new Payment(1L, "28a8c1e3bc2742d8848569891fb42181"));
+        hashMap.put(2L, new Payment(2L, "28a4c1e3bc2742d8848569891fb42181"));
+        hashMap.put(3L, new Payment(3L, "28a5c1e3bc2742d8848569891fb42181"));
+    }
+
+    @GetMapping(value = "/paymentSQL/{id}")
+    public CommonResult<Payment> paymentSQL(@PathVariable("id") Long id) {
+        Payment payment = hashMap.get(id);
+        CommonResult<Payment> result = new CommonResult<>(200, "from mysql,serverPort: " + serverPort, payment);
+        return result;
+    }
+
+}
