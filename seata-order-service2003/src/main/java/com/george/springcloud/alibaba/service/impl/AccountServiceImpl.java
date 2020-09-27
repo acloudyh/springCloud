a9a30d1eb3dce9eb565c6c0fd5f99d3a1a9ef172
YangHao <acloudyh@gmail.com>
2020-09-27 21:26:54 +0800

    fix

--- seata-order-service2003/src/main/java/com/george/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-order-service2003/src/main/java/com/george/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -28,11 +28,11 @@ public class AccountServiceImpl implements AccountService {
         //  模拟异常 配合 2001 服务里面的注解    @GlobalTransactional(name = "fsp-create-order", rollbackFor = Exception.class)
         //  导致结果,订单状态没变成已完成,库存和账户都被扣减
         //  正常测试注销try catch.
-        try {
-            TimeUnit.SECONDS.sleep(20);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            TimeUnit.SECONDS.sleep(20);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         accountDao.decrease(userId, money);
         log.info("------------->account-service 中扣减库存结束");
     }
