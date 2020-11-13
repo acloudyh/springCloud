41760f9507dc44a0c5b6f72b9d6172f5bfdba882
acloudyh <acloudyh@gmail.com>
2020-11-13 22:05:32 +0800

    集群设置

--- cloud-consumer-order80/src/main/java/com/george/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/OrderMain80.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class)
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/george/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/controller/OrderController.java
@@ -23,7 +23,7 @@ import java.util.List;
 @Slf4j
 public class OrderController {
 
-    //    public static final String PAYMENT_URL = "http://localhost:8001";
+    //        public static final String PAYMENT_URL = "http://localhost:8001";
     //集群负载均衡
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     @Resource
