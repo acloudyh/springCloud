7c17e8607763ac8d07f62a10b35341919ac470d6
acloudyh <acloudyh@gmail.com>
2020-11-13 14:19:20 +0800

    payment

---
+++ payment.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `payment`
+(
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `serial` varchar(128) DEFAULT '',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 16
+  DEFAULT CHARSET = utf8;
+
+
