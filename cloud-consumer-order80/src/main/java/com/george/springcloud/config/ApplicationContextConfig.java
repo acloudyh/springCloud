571c2a5b63bd338c38bc1c073337ded3180136b5
acloudyh <acloudyh@gmail.com>
2020-11-24 14:17:21 +0800

    为了试验自己写的rule

--- cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,5 @@
 package com.george.springcloud.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
-    @LoadBalanced //为了试验自己写的rule
+//    @LoadBalanced //为了试验自己写的rule
     //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
