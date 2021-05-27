93aa4be1077bf3f39c5f22db6bd44d1afd87714b
acloudyh <acloudyh@gmail.com>
2021-05-27 16:06:00 +0800

    自定义规则

--- cloud-consumer-order80/src/main/java/com/george/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.george.springcloud;
 
+import com.george.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author Yang Hao
@@ -10,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class)
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyselfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/george/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,5 @@
 package com.george.springcloud.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
-    @LoadBalanced //为了试验自己写的rule
+//    @LoadBalanced //注销掉是 为了试验自己写的rule,此时其它接口暂时不能用,测试方法 http://localhost/consumer/payment/lb
     //使用@LoadBalanced注解赋予RestTemplate负载均衡的能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
